d79df8a9ef657f74e07d8a057ed1775b26cc967d
Sam Biondolillo <biwinning121@gmail.com>
2016-06-26 19:31:42 +0000

    Complete sorting based on title and release date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort = params[:sort] || :id
+    @movies = Movie.order(@sort)
   end
 
   def new
